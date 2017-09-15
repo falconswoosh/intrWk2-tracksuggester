365e36799e977c362986cd4e9aa1ad8a41f4d139
af <falconswoosh@gmail.com>
2017-09-15 11:19:54 -0700

    test alternate input values in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-  $("form").submit(function(event) {
-  var frontEnd = document.getElementById("gridRadios1");
-  var backEnd = document.getElementById("gridRadios2");
-
-  });
-  alert(frontEnd);
-  alert(backEnd);
-  event.preventDefault();
+//  $("form").submit(function(event) {
+//  var frontEndInput = document.getElementById("gridRadios1").val();
+//  var backEndInput = document.getElementById("gridRadios2").val();
+  var frontEndInput = $("input#gridRadios1").val();
+  var backEndInput = $("input#gridRadios2").val();
+//  });
+  alert(frontEndInput);
+  alert(backEndInput);
+//  event.preventDefault();
 });
