26b6062e9c73a45099e1a90ceabdf3d3bf8af362
af <falconswoosh@gmail.com>
2017-09-15 16:17:57 -0700

    add show and hide of text in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-  alert("start");
+  //alert("start");  (commentByAF: Use this to test this code starts as expected.)
   var frontEndInput1 = document.getElementById("gridRadios1");
   var backEndInput1 = document.getElementById("gridRadios2");
   var notSureInput1 = document.getElementById("gridRadios3");
@@ -13,14 +13,24 @@ $(document).ready(function() {
   var frontEndInput4 = document.getElementById("gridRadios11");
   var backEndInput4 = document.getElementById("gridRadios10");
   var notSureInput4 = document.getElementById("gridRadios12");
-  event.preventDefault();
+  //event.preventDefault();
   if ((frontEndInput1).checked && (frontEndInput2).checked && (frontEndInput3).checked && (frontEndInput4).checked) {
-    alert("You choose to be a frontend programmer. We recommend either our CSS/Design track or Java/Android.");
+    //alert("You choose to be a frontend programmer. We recommend either our CSS/Design or Java/Android track.");
+    $('#frontEndResult').show();
+    $('#backEndResult').hide();
+    $('#noClearChoiceResult').hide();
   } else if ((backEndInput1).checked && (backEndInput2).checked && (backEndInput3).checked && (backEndInput4).checked) {
-    alert("You choose to be a backend programmer. We recommend either ");
+    //alert("You choose to be a backend programmer. We recommend either Ruby/Rails, PHP/Drupal, or C$/.NET tracks");
+    $('#backEndResult').show();
+    $('#frontEndResult').hide();
+    $('noClearChoiceResult').hide();
   } else {
-    alert("We recommend you start with any one of the tracks since they all begin with Intro to Programming and Javascript. Prior to completing these two requirements, you may decide which track offered is more your passion.")
+    //alert("We recommend you start with any one of the tracks since they all begin with Intro to Programming and Javascript. Prior to completing these two requirements, you may decide which specific track available is more your passion.")
+    $('noClearChoiceResult').show();
+    $('#backEndResult').hide();
+    $('#frontEndResult').hide();
   }
-  alert("end");
+  //alert("end"); (commentByAF: Use this to test this code if it gets to the end.)
+  event.preventDefault();
     });
 });
