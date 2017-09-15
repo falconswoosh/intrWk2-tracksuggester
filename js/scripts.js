2b3d9f1eebfd11cd0c43f327a2c54fa6990a710d
af <falconswoosh@gmail.com>
2017-09-15 12:41:00 -0700

    fix 2nd logic for 2nd set of questions in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,15 @@ $(document).ready(function() {
   alert("start");
   var frontEndInput1 = document.getElementById("gridRadios1");
   var backEndInput1 = document.getElementById("gridRadios2");
-  var frontEndInput2 = document.getElementById("gridRadios3");
-  var backEndInput2 = document.getElementById("gridRadios4");
+  var frontEndInput2 = document.getElementById("gridRadios4");
+  var backEndInput2 = document.getElementById("gridRadios3");
   event.preventDefault();
-  if ((frontEndInput1).checked && (frontEndInput2)) {
+  if ((frontEndInput1).checked && (frontEndInput2).checked) {
     alert("You choose to be a frontend programmer");
-  } else if ((backEndInput1).checked && (backEndInput2)) {
+  } else if ((backEndInput1).checked && (backEndInput2).checked) {
     alert("You choose to be a backend programmer");
   } else {
+    alert("You choose to be a full-stack programmer")
   }
   alert("end");
     });
