3da722643b7f5c3afe51a21469af6559f6f18d68
af <falconswoosh@gmail.com>
2017-09-15 14:28:04 -0700

    add 4th set of logic for 4th set of questions in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,16 @@ $(document).ready(function() {
   alert("start");
   var frontEndInput1 = document.getElementById("gridRadios1");
   var backEndInput1 = document.getElementById("gridRadios2");
-
   var frontEndInput2 = document.getElementById("gridRadios3");
   var backEndInput2 = document.getElementById("gridRadios4");
   var frontEndInput3 = document.getElementById("gridRadios6");
   var backEndInput3 = document.getElementById("gridRadios5");
+  var frontEndInput4 = document.getElementById("gridRadios8");
+  var backEndInput4 = document.getElementById("gridRadios7");
   event.preventDefault();
-  if ((frontEndInput1).checked && (frontEndInput2).checked && (frontEndInput3).checked) {
+  if ((frontEndInput1).checked && (frontEndInput2).checked && (frontEndInput3).checked && (frontEndInput4).checked) {
     alert("You choose to be a frontend programmer");
-  } else if ((backEndInput1).checked && (backEndInput2).checked && (backEndInput3).checked) {
+  } else if ((backEndInput1).checked && (backEndInput2).checked && (backEndInput3).checked && (backEndInput4).checked) {
     alert("You choose to be a backend programmer");
   } else {
     alert("You choose to be a full-stack programmer")
