53af717c597c9e556ddfd2d5a3fe3726ef6c01aa
af <falconswoosh@gmail.com>
2017-09-15 15:19:30 -0700

    add logic for 3rd question in all question sets in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,16 @@ $(document).ready(function() {
   alert("start");
   var frontEndInput1 = document.getElementById("gridRadios1");
   var backEndInput1 = document.getElementById("gridRadios2");
-  var frontEndInput2 = document.getElementById("gridRadios3");
-  var backEndInput2 = document.getElementById("gridRadios4");
-  var frontEndInput3 = document.getElementById("gridRadios6");
-  var backEndInput3 = document.getElementById("gridRadios5");
-  var frontEndInput4 = document.getElementById("gridRadios8");
-  var backEndInput4 = document.getElementById("gridRadios7");
+  var notSureInput1 = document.getElementById("gridRadios3");
+  var frontEndInput2 = document.getElementById("gridRadios4");
+  var backEndInput2 = document.getElementById("gridRadios5");
+  var notSureInput2 = document.getElementById("gridRadios6");
+  var frontEndInput3 = document.getElementById("gridRadios8");
+  var backEndInput3 = document.getElementById("gridRadios7");
+  var notSureInput3 = document.getElementById("gridRadios9");
+  var frontEndInput4 = document.getElementById("gridRadios11");
+  var backEndInput4 = document.getElementById("gridRadios10");
+  var notSureInput4 = document.getElementById("gridRadios12");
   event.preventDefault();
   if ((frontEndInput1).checked && (frontEndInput2).checked && (frontEndInput3).checked && (frontEndInput4).checked) {
     alert("You choose to be a frontend programmer");
