7aa1fb344bd39e7316e5ba5be80cfbeba3e3ba64
af <falconswoosh@gmail.com>
2017-09-15 11:09:36 -0700

    add submit button

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   $("form").submit(function(event) {
   var frontEnd = document.getElementById("gridRadios1");
   var backEnd = document.getElementById("gridRadios2");
-  event.preventDefault();
+
   });
   alert(frontEnd);
   alert(backEnd);
+  event.preventDefault();
 });
