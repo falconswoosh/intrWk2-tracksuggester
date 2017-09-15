17248a55163191dfab8851486d923a014ab11ad1
af <falconswoosh@gmail.com>
2017-09-15 11:53:55 -0700

    add code in scripts.js to test if radio is checked

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,13 @@ $(document).ready(function() {
   var frontEndInput = document.getElementById("gridRadios1");
   var backEndInput = document.getElementById("gridRadios2");
   event.preventDefault();
+  if frontEndInput.checked {
+    alert("You choose to be a frontend programmer");
+  } else {
+    alert("You choose to be a backend programmer");
+  }
   alert("end");
-  });
+    });
   //alert(frontEndInput.checked);
   //alert(backEndInput.checked);
 });
