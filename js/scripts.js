a8fefc24c2ba609ebfda2ade2ce58d5ddc85f116
af <falconswoosh@gmail.com>
2017-09-15 12:30:20 -0700

    add 2nd logic for 2nd set of questions into scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
   alert("start");
-  var frontEndInput = document.getElementById("gridRadios1");
-  var backEndInput = document.getElementById("gridRadios2");
+  var frontEndInput1 = document.getElementById("gridRadios1");
+  var backEndInput1 = document.getElementById("gridRadios2");
+  var frontEndInput2 = document.getElementById("gridRadios3");
+  var backEndInput2 = document.getElementById("gridRadios4");
   event.preventDefault();
-  if ((frontEndInput).checked) {
+  if ((frontEndInput1).checked && (frontEndInput2)) {
     alert("You choose to be a frontend programmer");
-  } else {
+  } else if ((backEndInput1).checked && (backEndInput2)) {
     alert("You choose to be a backend programmer");
+  } else {
   }
   alert("end");
     });
